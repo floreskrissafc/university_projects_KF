c3022a9648ccb35e18bc8cdf5eff783061c69db1
Kriss Flores <floreskrissafc@gmail.com>
2026-03-01 20:24:44 -0800

    Update dataForBarChart.js
    added forfeited amount

--- INRAV/dataForBarChart.js
+++ INRAV/dataForBarChart.js
@@ -2,515 +2,590 @@ export const cases = [{
         accused: 'Víctor Fossi Grieco',
         moneyAmount: 6452880,
         caseID: "0:19-cr-60280-CMA",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: 'Álvaro Pulido Vargas, José Gregorio Vielma Mora, Emmanuel Enrique Rubio González, Carlos Rolando Lizcano Manrique, Ana Guillermo Luis',
         moneyAmount: 1607626307.19,
         caseID: "21-cr-20509",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: 'Álex Naim Saab Morán y Álvaro Pulido Vargas',
         moneyAmount: 362692197.66,
         caseID: "19-cr-20450",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: 'Carmelo Antonio Urdaneta Aquí',
         moneyAmount: 49265050.85,
         caseID: "1:18-cr-20685-KMW",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 5000000
 },
 
     {
         accused: "Natalino D'Amato",
         moneyAmount: 45986230.1,
         caseID: "1:20-cr-20241-KMW",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 45986230.1,
 },
 
     {
         accused: "Francisco Illarramendi",
         moneyAmount: 25844834,
         caseID: "3:11-cr-0041",
-        state: "CT"
+        state: "CT",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "David Rivera y Esther Nuhfer",
         moneyAmount: 23750000,
         caseID: "3:11-cr-0041",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Ernesto Luján",
         moneyAmount: 18514560,
         caseID: "13-cr-00671",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "José Alejandro Hurtado",
         moneyAmount: 11896743,
         caseID: "13-cr-673",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Darwin Enrique Padron-Acosta",
         moneyAmount: 9052397.73,
         caseID: "16-cr-00437",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "José Luis Ramos Castillo",
         moneyAmount: 7812315.15,
         caseID: "4:15-cr-00636",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: 210625.79
 },
 
     {
         accused: "Tomás Alberto Clarke Bethancourt",
         moneyAmount: 5787824,
         caseID: "13-cr-670",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Jesús Ramón Veroes",
         moneyAmount: 5500000,
         caseID: "19-cr-20351",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 1100000
 },
 
     {
         accused: "Luis Alberto Chacín Haddad",
         moneyAmount: 5500000,
         caseID: "1:19-CR-20351-CMA",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Lennys Lisoleth Rangel Soto",
         moneyAmount: 5000000,
         caseID: "1:19-cr-20726-JEM",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 256829.14 
 },
 
     {
         accused: "Edoardo Orsoni",
         moneyAmount: 4500000,
         caseID: "19-cr-20725",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Fernando Ardila Rueda",
         moneyAmount: 4469756.04,
         caseID: "17-cr-00515",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: 1116038.42
 },
 
     {
         accused: "Benito Chinea",
         moneyAmount: 3636432,
         caseID: "1:14-cr-00240-DLC",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Jorge Nóbrega",
         moneyAmount: 3783337.93,
         caseID: "1:21-cr-20442-PCH-1/",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Karina del Carmen Nuñez Arias",
         moneyAmount: 3238720.19,
         caseID: "16-cr-00436",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Juan José Hernández Comerma",
         moneyAmount: 3000000,
         caseID: "17-cr-00005",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Carlos Enrique Urbano Fermín",
         moneyAmount: 3000000,
         caseID: "20-cr-20163",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Hjalmar Gibelli Gómez, Fabrizio Della Polla De-Simone",
         moneyAmount: 1820981.05,
         caseID: "4:17-cv-02920-SNLJ",
-        state: "MO"
+        state: "MO",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Tulio Aníbal Farías",
         moneyAmount: 1500000,
         caseID: "20-cr-00089",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "José Orlando Camacho",
         moneyAmount: 1318648,
         caseID: "17-cr-00394",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: 1318648
 },
 
     {
         accused: "Juan Carlos Horna Napolitano",
         moneyAmount: 1250000,
         caseID: "3:11-cr-00086-SRU",
-        state: "CT"
+        state: "CT",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Juan Carlos Castillo Rincón",
         moneyAmount: 1061000,
         caseID: "18-cr-00200",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Jaime Alberto Marin Zamora",
         moneyAmount: 1000000,
         caseID: "09-CR-20140",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Rafael Enrique Pinto-Franceschi",
         moneyAmount: 985416.6,
         caseID: "19-CR-00135",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Iván Alexis Guédez",
         moneyAmount: 978339.5,
         caseID: "4:18-cr-00611",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: 978339.5
 },
 
     {
         accused: "Charles Quintard Beach III",
         moneyAmount: 833452,
         caseID: "17-cr-00006",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Alfonso Eliezer Gravina Muñoz",
         moneyAmount: 590446,
         caseID: "15-cr-00637",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Alejandro Javier Marín",
         moneyAmount: 547911,
         caseID: "1:19-cr-00144-AKH",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: 547911
 },
 
     {
         accused: "Moisés Abraham Millan Escobar",
         moneyAmount: 533578.13,
         caseID: "16-cr-009",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Bruce Bagley",
         moneyAmount: 474000,
         caseID: "1:19-cr-00765-JSR",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Laymar Giosse Peña Torrealba",
         moneyAmount: 332308.47,
         caseID: "4:19-cr-00186",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Juan Carlos Guillén Zerpa",
         moneyAmount: 315000,
         caseID: "16-17407",
-        state: "CT"
+        state: "CT",
+        forfeitedAmount: 315000
 },
 
     {
         accused: "Benny Palmeri Bacchi",
         moneyAmount: 300000,
         caseID: "1:13-cr-20930-UU",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 179484.28
 },
 
     {
         accused: "Franz Herman Muller-Huber",
         moneyAmount: 263402.83,
         caseID: "19-CR-00135",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Franklin Durán",
         moneyAmount: 175000,
         caseID: "1:07-cr-20999-JAL",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Christian Javier Maldonado Barillas",
         moneyAmount: 165000,
         caseID: "15-cr-00635",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Joseph DeMeneses",
         moneyAmount: 2670612,
         caseID: "1:14-cr-00240-DLC",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "María De Los Ángeles González De Hernández",
         moneyAmount: 8347849,
         caseID: "1:13-cr-00901-PAE",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Darwin Enrique Padron-Acosta",
         moneyAmount: 9052397.73,
         caseID: "4:16-cr-00437",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Alejandro José Andrade Cedeño",
         moneyAmount: 1000000000,
         caseID: "17-cr-80242",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"      
 },
 
     {
         accused: "Gabriel Arturo Jiménez Aray",
         moneyAmount: 38000000,
         caseID: "18-cr-80054",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Raul Gorrin Belisario",
         moneyAmount: 79000000,
         caseID: "18-cr-80160",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Claudia Patricia Diaz Guillen",
         moneyAmount: 136752007.46,
         caseID: "18-cr-80160",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 1291902.66
 },
 
     {
         accused: "Naman Wakil",
         moneyAmount: 50000000,
         caseID: "1:21-cr-20406-KMW",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Roberto de Jesús Soto García",
         moneyAmount: 206250,
         caseID: "1:15-cr-00765-PAC",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Abraham Edgardo Ortega",
         moneyAmount: 12000000,
         caseID: "1:18-cr-20685-KMW",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 839803.33
 },
 
     {
         accused: "Francisco Convit",
         moneyAmount: 45585667.46,
         caseID: "18-cr-20685",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Gustavo Adolfo Hernández Frieri",
         moneyAmount: 12300000,
         caseID: "1:18-cr-20685-KMW",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 3938705.3
 },
 
     {
         accused: "Matthias Krull",
         moneyAmount: 600000,
         caseID: " 1:18-cr-20682-CMA",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Álvaro Ledo Nass",
         moneyAmount: 11510425,
         caseID: "1:23-cr-20089-KMW",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "José Luis De Jongh Atencio",
         moneyAmount: 8887759.62,
         caseID: "20-cr-00305",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: 157501.42
 },
 
     {
         accused: "Abraham José Shiera Bastidas",
         moneyAmount: 19269770.05,
         caseID: "4:15-cr-00654",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Roberto Enrique Rincón Fernández",
         moneyAmount: 51251219.23,
         caseID: "15-cr-00654",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "César David Rincón Godoy",
         moneyAmount: 1212905.87,
         caseID: "4:17-cr-00514",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: 1212905.87
 },
 
     {
         accused: "Luis Carlos De León Pérez",
         moneyAmount: 18118783.15,
         caseID: "17-cr-514",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Jhonnathan Teodoro Marin Sanguino",
         moneyAmount: 3800000,
         caseID: "1:22-cr-20164-RNS",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Luis Díaz Jr y Luis Javier Díaz",
         moneyAmount: 101346285.42,
         caseID: "1:17-cr-00077-WHP",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Rixon Moreno Oropeza",
         moneyAmount: 32000000,
         caseID: "22-cr-20391",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "Orlando Alfonso Contreras Saab",
         moneyAmount: 5947188,
         caseID: "23-cr-20364",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
 
     {
         accused: "José Manuel González Testino",
         moneyAmount: 0,
         caseID: "4:19-cr-00341",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: 14000000
 },
 
     {
         accused: "Gonzalo Jose Jorge Morales-Divo",
         moneyAmount: 2534365.65,
         caseID: "4:19-cr-00148",
-        state: "TX"
+        state: "TX",
+        forfeitedAmount: "unknown"
 },
     {
         accused: "Telefonica Venezolana C.A.",
         moneyAmount: 85260000,
         caseID: "1:24-cr-00633-DEH",
-        state: "NY"
+        state: "NY",
+        forfeitedAmount: "unknown"
 },
     {
         accused: "Luis Fernando Vuttef",
         moneyAmount: 4124722,
         caseID: "1:22-cr-20306-DPG",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
     {
         accused: "Naman Wakil",
         moneyAmount: 21248434.25,
         caseID: "23-cv-22987",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
     {
         accused: "Villalobos, Della Polla, Blackforest",
         moneyAmount: 8000000,
         caseID: "24-cv-001714",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 8000000
 },
     {
         accused: "George Semerene",
         moneyAmount: 52647.02,
         caseID: "21-CR-20589",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 52647.02
 },
     {
         accused: "Maikel Moreno Perez",
         moneyAmount: 0,
         caseID: "23-CR-20035",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: "unknown"
 },
     {
         accused: "Nepmar Jesus Escalona Enriquez",
         moneyAmount: 841649.88,
         caseID: "22-CR-20423",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 841649.88
 },
     {
         accused: "Jose Vicente Amparan Croquer",
         moneyAmount: 40405.72,
         caseID: "18-CR-20685",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 40405.72
 },
     {
         accused: "Leonardo Santilli Garcia",
         moneyAmount: 43769169.48,
         caseID: "21-CV-20614",
-        state: "FL"
+        state: "FL",
+        forfeitedAmount: 43769169.48
 }                    
 ];
